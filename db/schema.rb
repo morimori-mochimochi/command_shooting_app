6e1f30c331919e3c09c4eff1e1dbce4cc3331603
morimori-mochimochi <reveluv.chino1205@icloud.com>
2026-03-03 12:23:10 +0900

    マイグレーション後

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_22_090116) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_03_022732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_22_090116) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "duration"
+    t.integer "command_ids", default: [], array: true
     t.index ["user_id"], name: "index_game_sessions_on_user_id"
   end
 
