5ef3675b84e612f293e7c8785f89238151916e59
morimori-mochimochi <reveluv.chino1205@icloud.com>
2026-03-04 21:46:31 +0900

    コマンドを一つずつ表示するロジックをヘルパーに切り出す

--- app/helpers/game_sessions_helper.rb
+++ app/helpers/game_sessions_helper.rb
@@ -1,2 +1,12 @@
 module GameSessionsHelper
+  # 次の問題、もしくは終了画面へのURLを返すヘルパーメソッド
+  def next_question_url(game_session, commands, current_index)
+    if commands.size > current_index + 1
+      # 次の問題がある場合
+      game_session_path(game_session, index: current_index + 1)
+    else
+      # これが最後の問題だった場合
+      finished_game_session_path(game_session)
+    end
+  end
 end
